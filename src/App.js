e8a544d320189d57c2bc1ecb8705249b64faf622
Norayr Hovhannisyan <hovhannisyan_norayr@inbox.ru>
2020-12-24 23:21:48 +0400

    homework4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './header';
 import Index from './index';
 import Footer from './footer';
 import {Product} from './Product';
+import {ToDoList} from './ToDoList';
+
 
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
         <Header name="header.js"/>
         <Footer name="footer.js"/>
         <Product name='bananas ' price='12$' description='Fresh bananas from Ecuador ' /> 
+        <ToDoList/>
       </header>
     </div>
   );
